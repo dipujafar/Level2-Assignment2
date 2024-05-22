49c665f55bf5ffdd81c70a93d5d0f1c1fd756326
dipujafar <dipujafarmr@gmail.com>
2024-05-22 15:02:20 +0600

    create api for create order

---
+++ src/app/modules/order/order.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { orderService } from './order.service';
+
+const createOrder = async (req: Request, res: Response) => {
+  try {
+    const orderData = req.body;
+
+    const result = await orderService.createOrderIntoDB(orderData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Order created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
+};
+
+export const orderController = {
+  createOrder,
+};
